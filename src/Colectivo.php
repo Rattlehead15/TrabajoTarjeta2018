a6d28c8997fe4f8144510789d66ad88025fe840f
Rattlehead15 <lautaro.123@live.com.ar>
2018-08-08 19:42:47 -0300

    Añadidas implementaciónes de la primera iteración. No baja el dinero de la tarjeta al pagar porque no se pidió en ningún lado que se haga eso

---
+++ src/Colectivo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TrabajoTarjeta;
+
+class Colectivo implements ColectivoInterface{
+    protected $linea;
+    protected $empresa;
+    protected $numero;
+
+    public function linea(){
+        return $this->linea;
+    }
+
+    public function empresa(){
+        return $this->empresa;
+    }
+
+    public function numero(){
+        return $this->numero;
+    }
+
+    public function pagarCon(TarjetaInterface $tarjeta){
+        if($tarjeta->obtenerSaldo() >= 14.80){
+            return new Boleto(14.80, $this, $tarjeta);
+        }
+        return false;
+    }
+}

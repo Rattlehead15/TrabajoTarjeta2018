7e564992127dd4c9606b63b2e7ca35267f7c1c49
MarcosCassinerio <nanocassi47@gmail.com>
2018-08-30 23:59:42 -0300

    agregue que se pagara segun el precio con el que se paga

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -18,14 +18,18 @@ class ColectivoTest extends TestCase {
         $bondi = new Colectivo();
         $tarjeta = new Tarjeta();
         $tarjeta->recargar(20);
+        $valordebido = 20 - $tarjeta->precio;
         $this->assertEquals($bondi->pagarCon($tarjeta), new Boleto(14.80, $bondi, $tarjeta));
-        $this->assertEquals($tarjeta->obtenerSaldo(), 5.2);
+        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
         $this->assertEquals($tarjeta->obtenerPlus(), 0);
         $this->assertEquals($bondi->pagarCon($tarjeta), new Boleto(14.80, $bondi, $tarjeta));
+        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
         $this->assertEquals($tarjeta->obtenerPlus(), 1);
         $this->assertEquals($bondi->pagarCon($tarjeta), new Boleto(14.80, $bondi, $tarjeta));
+        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
         $this->assertEquals($tarjeta->obtenerPlus(), 2);
         $this->assertEquals($bondi->pagarCon($tarjeta), false);
+        $this->assertEquals($tarjeta->obtenerSaldo(), $valordebido);
         $this->assertEquals($tarjeta->obtenerPlus(), 2);
     }
 }
